e5edff6a855071b833606ba6b52c79a90495371c
Mejri Rached <r.mejri74100@gmail.com>
2020-10-24 16:42:17 +0200

    feat: adding a CLI struct

---
+++ src/cli.rs
@@ -0,0 +1,23 @@
+extern crate clap;
+
+use clap::{App, Arg, SubCommand};
+
+pub struct CLI;
+
+impl CLI {
+    pub fn new<'a>() -> clap::ArgMatches<'a> {
+        let arg1: Arg = Arg::with_name("config")
+            .short("c")
+            .long("config")
+            .value_name("FILE")
+            .help("Sets a custom config file")
+            .takes_value(true);
+
+        App::new("Blockshain client interface")
+            .version("0.1")
+            .author("Rached MEJRI <r.mejri74100@gmail.com")
+            .about("")
+            .arg(arg1)
+            .get_matches()
+    }
+}
