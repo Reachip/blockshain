94e3b052cf5fd9be7b599c6f31d05a8e05241b16
Rached <r.mejri74100@gmail.com>
2021-07-13 13:40:15 +0200

    feat: add main file

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,34 @@ use client::Client;
 use crate::network::signal::Signal;
 use std::path::PathBuf;
 use std::borrow::Borrow;
+use crate::client_reader::UnixSocketReader;
 
 mod block;
 mod client;
 mod network;
 mod cli;
 mod hash;
+mod client_reader;
+mod client_writer;
+
+use client_reader::ClientReader;
+use client_writer::ClientWriter;
+use crate::client_writer::UnixSocketWriter;
 
 fn main() {
-    /*let clients_sockets_location = match env::var("BLOCKSHAIN_SOCKETS_LOCATION") {
+    let clients_sockets_location = match env::var("BLOCKSHAIN_SOCKETS_LOCATION") {
         Ok(var) => var,
         Err(_) => {
             panic!("BLOCKSHAIN_SOCKETS_LOCATION didn't provided so we use /tmp location, make sure that you have permission");
         }
-    };*/
+    };
 
-
-    let clients_sockets_location = "/home/rached/bs/".to_owned();
     let client = Arc::new(Client::new(clients_sockets_location.clone()).unwrap());
-    let client_responder = client.clone();
-    let client_reader = client.clone();
+
+    let client_responder = ClientWriter::new(client.clone());
+    let client_reader = ClientReader::new(client.clone());
+
+    println!("{} created",  client.client_socket_path);
 
     let client_reader_task = thread::spawn(move || {
         loop {
@@ -38,13 +46,12 @@ fn main() {
     });
 
     let client_responder_task = thread::spawn(move || {
-        loop {    
-            client_responder.respond_to_node(Signal::is_okay(&(*client_responder), true)).unwrap();
+        loop {
+            client_responder.respond_to_node(Signal::is_okay(&*client.clone(), true)).unwrap();
             thread::sleep(time::Duration::from_nanos(1));
         }
     });
 
-   
     client_reader_task.join().unwrap();
     client_responder_task.join().unwrap();
 }
